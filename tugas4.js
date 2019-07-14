7d3b0e954cecc41500141e59f0b167466561fc6c
yohanes_prima <jooprima@gmail.com>
2019-07-14 22:33:41 +0700

    Tugas 4 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas4.js
@@ -0,0 +1,18 @@
+var a = 120;
+var b = 178;
+var c = 137;
+
+if (a > b) {
+  if (a > c) {
+    console.log("a tertinggi");
+  }
+} else if (b > a) {
+  if (b > c) {
+    console.log("b tertinggi");
+  } else {
+    console.log("c tertinggi");
+  }
+}
+
+// console.log("b tertinggi pertama = " + b);// console.log("c tertinggi kedua = " + c);
+// console.log("a tertinggi ketiga = " + a);
